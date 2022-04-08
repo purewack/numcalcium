87ca4b0338f596489226e8a091db03fb0989c822
purewack <54288625+purewack@users.noreply.github.com>
2022-04-08 15:23:18 +0100

    calc specs

--- numcalc/numcalc.ino
+++ numcalc/numcalc.ino
@@ -1,12 +1,36 @@
 
 //CMODE:
 //numpad -> F1 = normal, F2 = Arrows, F3 = phone ascii
+
 //calculator -> F1 = basic, F2 = sci, F3 = comp
+  //calc ops
+  //(,)
+  //+,-,/,*
+  //^, log10, log2, loge, sqrt 
+  //e, pi,
+  //abs
+  //(a)sin, (a)cos, (a)tan, (hyp)
+  //
+  //comp
+  //(,)
+  //+,-,/,* integers
+  //<<,>>
+  //&,|,!,^
+  //&&,||,
+  //%
+  //bit input
+
 //midi -> F1 = launchpad, F2 = seq usb, F3 = seq uart midi
+
 //comms monitor -> F1 = UART, F2 = SPI, F3 = I2C
+
 //GPIO -> F1 = debounced sw, F2 = clock bits program like cpld, F3 = PWM channels
+
 //PWM audio -> F1 = osc, F2 scope, F3 nodes
+
 //ws28xx ?
+
+
 
 //current stats:
 //rom 40% 26.2kb / 64kb 
