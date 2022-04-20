5b0ccf84e5ad277fc568cff8ec437ed32361b387
purewack <54288625+purewack@users.noreply.github.com>
2022-04-20 12:36:53 +0100

    prep for comms

--- numcalc/mode_comms.cpp
+++ numcalc/mode_comms.cpp
@@ -4,15 +4,23 @@
 #include "include/modes.h"
 
 void mode_comms_on_begin(){
-    
+#ifndef DEBUG
+    Serial.begin()
+#endif
+    SPI2.begin();
+    Wire.begin();
 }
 
 void mode_comms_on_end(){
-
+#ifndef DEBUG
+    Serial.end();
+#endif
+    SPI2.end();
+    Wire.end();
 }
 
 void mode_comms_on_press(int i){
-    
+
 }
 
 void mode_comms_on_release(int i){
@@ -20,5 +28,5 @@ void mode_comms_on_release(int i){
 }
 
 void mode_comms_on_gfx(){
-   
+
 }
