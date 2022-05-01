a082de1d05aefcfc0c0770f072819f06157f9dcf
purewack <bobimaster15@gmail.com>
2022-05-01 18:56:28 +0100

    vnum_t insert and delete digits

--- numcalc/include/number.h
+++ numcalc/include/number.h
@@ -2,19 +2,17 @@
 #include "util.h"
 
 struct vnum_t{
-    int8_t m_dc;
-    int8_t e_dc;
+    uint32_t m_dc;
+    uint32_t e_dc;
     uint32_t e_int;
     uint32_t m_int;
+    bool dot;
     double result;
 };
 
-extern vnum_t keypad_num;
-extern bool keypad_num_inputting;
-extern bool keypad_num_dot;
-
-void beginNumberInput(vnum_t &n);
-void endNumberInput(vnum_t &n);
-void numberInputKey(vnum_t& n, uint32_t i);
-bool numberInputBackspace(vnum_t &n);
-void print_vnum(vnum_t& n, int& x, const int y);
+double computeNumber(vnum_t &n);
+void clearNumber(vnum_t &n);
+void numberInputKey(vnum_t& n, uint32_t i, int pos);
+bool numberInputBackspace(vnum_t &n, int pos);
+void printNumber(vnum_t& n, int &x, const int y);
+int numberLength(vnum_t& n);
