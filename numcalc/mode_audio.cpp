c2701ecc09d076fb5fd96650d1333680f958632d
purewack <bobimaster15@gmail.com>
2022-07-12 13:21:29 +0100

    add libintdsp and begin function outlines

--- numcalc/mode_audio.cpp
+++ numcalc/mode_audio.cpp
@@ -1,1 +1,25 @@
 #include "include/audio.h"
+#include "include/sys.h"
+#include "include/comms.h"
+#include "include/modes.h"
+struct sig_gen_t
+{
+    double f;
+    double a;
+    int shape;
+    int16_t table[512];
+} sig_gen;
+
+void mode_audio_on_begin(){
+
+}
+
+void mode_audio_on_process_end(){
+
+}
+
+void mode_audio_on_process(){
+    if(abuf.req){
+
+    }
+}
