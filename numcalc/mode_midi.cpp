29e6aef0b77020370e423f641c926110a8676a7f
Damian Nowacki <bobimaster15@gmail.com>
2022-12-13 14:55:50 +0000

    [main] USB midi CC

--- numcalc/mode_midi.cpp
+++ numcalc/mode_midi.cpp
@@ -4,6 +4,8 @@
 
 int midi_base = 0; //c3
 extern double th;
+int midi_cc = -1;
+char midi_ccs[16];
 
 void mode_midi_on_begin(){
   lcd_clear();
@@ -30,10 +32,20 @@ void mode_midi_on_end(){
 void mode_midi_draw_base(){
     clearProgGFX();
       char str[32];
-    snprintf(str,32,"MIDI Base: %d",midi_base);
-    lcd_drawString(20,20,sys_font,str);
-    snprintf(str,32,"  -      +");
-    lcd_drawString(0,64-8,sys_font,str);
+    if(midi_cc == -1){
+        snprintf(str,32,"MIDI Base: %d",midi_base);
+        lcd_drawString(20,20,sys_font,str);
+        snprintf(str,32,"  -      +      CC");
+        lcd_drawString(0,64-8,sys_font,str);
+    }
+    else{
+        snprintf(str,32,"MIDI CC: [%d] Val(%d)",midi_cc,midi_ccs[midi_cc]);
+        lcd_drawString(0,20,sys_font,str);
+        snprintf(str,32,"                Note");
+        lcd_drawString(0,64-8,sys_font,str);
+        lcd_drawRectSize(0,64-32,128,4);
+        lcd_drawRectSize(0,64-31,midi_ccs[midi_cc],2);
+    }
     updateProgGFX();
 }
 
@@ -47,21 +59,49 @@ void mode_midi_on_process(){
         midi_base += 1;
         mode_midi_draw_base();
       }
+      else if(io.bscan_down & (1<<K_F3)){
+        if(midi_cc != -1)
+            midi_cc = -1;
+        else
+            midi_cc = 0;
+        mode_midi_draw_base();
+      }
       else{ 
         for(int i=0; i<20; i++){
-            if(io.bscan_down & (1<<i)) 
-                USB_midi.sendNoteOn(0,midi_base+i,127);
-            if(io.bscan_up & (1<<i)) 
-                USB_midi.sendNoteOff(0,midi_base+i,0);
+            if(midi_cc == -1){
+                if(io.bscan_down & (1<<i)) 
+                    USB_midi.sendNoteOn(0,midi_base+i,127);
+                if(io.bscan_up & (1<<i)) 
+                    USB_midi.sendNoteOff(0,midi_base+i,0);
+            }
+            else{
+                if(io.bscan_down & (1<<i)){
+                    midi_cc = i; 
+                    mode_midi_draw_base();
+                }
+            }
         }  
       }
       
+      if(midi_cc != -1 && (io.turns_left || io.turns_right)){
+        int turns = io.turns_right-io.turns_left;
+        int vv = int(midi_ccs[midi_cc])+turns;
+        if(vv < 0) vv = 0;
+        if(vv > 127) vv = 127; 
+        midi_ccs[midi_cc] = vv;
+        USB_midi.sendControlChange(0,midi_cc,vv);
+        io.turns_left = 0;
+        io.turns_right = 0;
+        mode_midi_draw_base();
+      }
+
       if(io.bscan_down) {
         io.bscan_down = 0;
       }
       if(io.bscan_up) {
         io.bscan_up = 0;
       }
+
     }
     else{
       clearProgGFX();
