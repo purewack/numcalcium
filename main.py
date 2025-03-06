4c563664f6c3828d52553ab96c3595cfbedb8ff6
purewack <bobimaster15@gmail.com>
2025-03-06 00:34:36 +0000

    launcher: list remote programs + refresh RT, list scrolling, keys navigation

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ def reset(lcd):
 
 
 def main():
-    os.chdir('/')
     board.clearLights()
     nav.shouldBack(0)
     if(keys == (keys.SHIFT | keys.A | keys.E | keys.F1)):
@@ -31,47 +30,59 @@ def main():
     lcd = board.LCD()
     nav.shouldBack(0)
     print("Standard Boot")
-    programs = collect_manifest_paths()
-    program_keys = list(programs.keys())
-    print(programs)
-    if not len(program_keys):
+
+    programs = findPrograms()
+    print(len(programs),[a['name'] for a in programs])  
+    if not len(programs):
         lcd.print("No Programs")
         return
 
     index = 0
-    def listPrograms(offset=0):
-        nonlocal programs, program_keys
+    offset = 0
+
+    def listPrograms(programs, offset=0):
         lcd.clear()
         lcd.scale(2)
-        lcd.fill(0,0,320,13*2,'#4a00c2')
+        lcd.fill(0,0,320,13*2,'#291475')
         lcd.color(lcd.WHITE)
-        lcd.background('#4a00c2')
-        lcd.print("P R O G R A M S")
+        lcd.background('#291475')
+        lcd.print("Apps - NumCalcium")
         lcd.background(lcd.BLACK)
-        for i in range(len(program_keys)):
-            lcd.cursor(2,i+1)
-            lcd.print(str(i + 1))
-            lcd.print(".")
-            lcd.print(program_keys[i])
+        for i in range(offset, min(offset + 5, len(programs))):
+            lcd.cursor(1.5,i+1.25-offset)
+            
+            rmt = programs[i]['remote']
+            lcd.background('#8aebb9' if rmt else lcd.BLACK)
+            lcd.color('#076334' if rmt else lcd.WHITE)
+            lcd.print(f"{'@' if rmt else ''}{programs[i]['name']}"[:18])
+            lcd.color(lcd.WHITE)
+            lcd.background(lcd.BLACK)
+        # if(len(programs) > 5 and offset < len(programs)-5):
+        #     lcd.scale(1)
+        #     lcd.cursor(8,12)
+        #     lcd.print("...")
     
-    def selectProgram(index):
+    def selectProgram(programs, index):
         lcd.scale(2)
         lcd.fill(0, 13*2, 8*2, 170-(13*2), lcd.BLACK)
-        for i in range(len(program_keys)):
+        for i in range(offset, min(offset + 5, len(programs))):
             if i == index:
-                lcd.cursor(0,i+1)
+                lcd.cursor(0,i+1.25-offset)
                 lcd.print(">")
 
-    listPrograms()
-    selectProgram(0)
+    listPrograms(programs)
+    selectProgram(programs,0)
+    if not len(programs):
+        return
 
     while True:
         turns = nav.turns()
-        if nav.wasBackRequested():  # Select and run program
+        if nav.wasBackRequested() or keys.isDown(keys.A):  # Select and run program
             while nav.shouldBack(): pass
+            keys.clearAll()
             
-            name = program_keys[index]
-            path = programs[program_keys[index]][0]
+            name = programs[index]['name']
+            path = programs[index]['path']
 
             sys.path.insert(0, path)
             
@@ -98,21 +109,29 @@ def main():
                 lcd.background('#e69600')
                 lcd.color(lcd.WHITE)
                 lcd.clear()
-                lcd.print(name," @",path)
+                lcd.cursor(0,2)
+                lcd.color(lcd.WHITE)
                 os.dupterm(lcd)
-                print(f"Failed in program [{name}]")
+                print(f"--Failed in program--\n\r{name} [{path}]")
+                lcd.cursor(0,5)
                 sys.print_exception(e)
                 os.dupterm(None)
-                lcd.print("\n\r Press any button to reset")
+                lcd.background('#c66600')
+                lcd.cursor(0,0)
+                lcd.print("Press any button to reset")
+
+                keys.clearAll()
                 while not (nav.shouldBack() or keys.isAnyDown()):
                     time.sleep(0.1)
                     board.statusLed(10,0,0)
                     time.sleep(0.1)
                     board.statusLed(0,0,0)
                 while nav.shouldBack(): pass
-                lcd.background(lcd.BLACK)
+                keys.clearAll()
 
             finally:
+                keys.clearAll()
+                while nav.shouldBack(): pass
                 print("----------------")
                 sys.path.pop(0)
                 sys.modules.clear()
@@ -121,55 +140,72 @@ def main():
                 gc.collect()
                 print("Post program - MEM (free,alloc):",gc.mem_free(), gc.mem_alloc())
                 
-                
             reset(lcd)
-            listPrograms()
-            selectProgram(index)
-            while nav.shouldBack(): pass
+            listPrograms(programs,offset)
+            selectProgram(programs, index)
 
-        elif turns:
-            index = (index + turns) % len(program_keys)
-            selectProgram(index)
-            keys.clearDown(keys.N5)
+        elif turns or keys.isDown(keys.N5) or keys.isDown(keys.N2):
+            prev_offset = offset
+            _index = -1 if turns < 0 else 1
+            if keys == keys.N2:
+                _index = 1
+            elif keys == keys.N5:
+                _index = -1
+            index = max(0,min((index + _index),len(programs)-1))
+            if index%5 == 0 and (turns > 0 or keys == keys.N2):
+                offset = offset + 5
+            elif index%5 == 4 and (turns < 0 or keys == keys.N5):
+                offset = max(0, offset - 5)
+            if prev_offset != offset:
+                listPrograms(programs,offset)
+            selectProgram(programs, index)
+            print(index,offset)
+            keys.clearAll()
+        
+        elif keys.isDown(keys.E):
+            try:
+                items = os.listdir('/remote')
+                programs = findPrograms()
+                offset = 0
+                index = 0
+                listPrograms(programs,offset)
+                selectProgram(programs, index)
+            except:
+                pass
+
         time.sleep(0.01)
 
-def collect_manifest_paths():
-    manifest_dict = {}
-    current_directory = os.getcwd()
-    print("looking in",current_directory)
+def collect_manifest_paths(base_path):
+    manifest_dict = []
     try:
-        items = os.listdir(current_directory)
+        items = os.listdir(base_path)
     except OSError:
-        return manifest_dict  # Return empty dict if directory doesn't exist
+        return manifest_dict
 
     for item in items:
-        item_path = current_directory + "/" + item
+        item_path = base_path + "/" + item
         try:
             stat = os.stat(item_path)
-            if stat[0] & 0x4000:  # Check if it's a directory
-                manifest_path = item_path + "/_manifest.txt"
-                manifest_content = None
-
+            if stat[0] & 0x4000:  # Directory check
                 program_path = item_path + "/_program.py"
-                progname = item
-
-                print(progname,item)
-                try:
-                    with open(manifest_path, "r") as file:
-                        manifest_content = file.readline().strip()  # Read program name
-                except:
-                    pass
                 try:
                     with open(program_path, "r") as file:
                         pass
-                    progname = item_path.split('/')[-1]
-                    manifest_dict[progname] = (item_path,program_path,manifest_content)
-                except:
+                    manifest_dict.append({
+                        'path':item_path,
+                        'name':item,
+                        'remote':item_path.startswith('/remote')
+                    })
+                except OSError:
                     pass
         except OSError:
             pass  # Ignore if directory or manifest doesn't exist
-
     return manifest_dict
+
+def findPrograms():
+    programs = collect_manifest_paths("/programs")
+    programs.extend(collect_manifest_paths("/remote"))
+    return programs
 
 def resetWatchdog():
     ticks = 0
